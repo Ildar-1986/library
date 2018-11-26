3a3930fdbb3686af4b2d7fb8018cd86650176a2c
Ваше Имя <you@example.com>
2018-11-26 13:07:06 +0300

    ttt

--- public/index.php
+++ public/index.php
@@ -1,32 +1,6 @@
 
 <?php
-//
-//$options = [
-//    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-//    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-//    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-//];
-//
-//$pdo = new PDO('mysql:host=localhost;dbname=library', 'local', '6', $options);
-//
-//
-//if ($pdo){
-//    echo  'success <br>';
-//}
-//else{
-//    echo 'error';
-//}
-//
-//
-//if (@$_POST['sql']) {
-//
-////echo '<br>' . "'{$a}'" .'<br>';
-//    $sth = $pdo->query(@$_POST['sql']);
-//    $res = $sth->fetchAll();
-//
-//    echo '<pre> '. print_r($res, true) .'</pre>';
-//
-//}
+
 
 
 use App\MySql;
@@ -45,7 +19,7 @@ $controller = $res[1] ?: 'Index';
 $class = '\App\controllers\\' . $controller;
 
 $controller = new $class();
-$controller();
+$controller();0
 
 
 
